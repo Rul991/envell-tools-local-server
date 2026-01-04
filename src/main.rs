687cc71d734af0f6bc404d2a0ae886c8aacd819d
Rul991 <archerartem6@gmail.com>
2026-01-04 19:51:03 +0300

    add custom port support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env::args;
+
 use axum::{routing::get, serve, Router};
 use tokio::{main, net::TcpListener};
 
@@ -8,8 +10,16 @@ mod utils;
 mod consts;
 mod routes;
 
+const DEFAULT_PORT: u16 = 3000;
+
 #[main]
 async fn main() {
+    let argv: &Vec<String> = &args().collect();
+    let default_value = format!("{DEFAULT_PORT}");
+
+    let arg_port = argv.get(1).unwrap_or(&default_value);
+    let port = arg_port.parse::<u16>().unwrap_or(DEFAULT_PORT);
+
     let app = Router::new()
         .route("/", get(index_handler))
         .route("/assets/{*file}", get(static_files_handler))
@@ -17,13 +27,13 @@ async fn main() {
         .route("/{instrument}", get(instrument_handler))
         .route("/default.css", get(default_css_handler));
         
-    let listener = TcpListener::bind("0.0.0.0:3000")
+    let listener = TcpListener::bind(format!("0.0.0.0:{port}"))
         .await
         .unwrap();
     
-    let addr = "127.0.0.1:3000";
+    let addr = format!("127.0.0.1:{port}");
     println!("Сервер запущен по адресу: http://{addr}\n");
-    print_instruments(addr);
+    print_instruments(&addr);
 
     serve(
         listener, 
